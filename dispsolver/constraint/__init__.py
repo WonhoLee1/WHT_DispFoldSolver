a6c3205de2bce2018c1ec91faf8674f35abe3925
WonhoLee1 <whbest.lee@gmail.com>
2026-07-02 02:00:11 +0900

    feat(constraint): export SpringHingeConstraint from constraint package
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- dispsolver/constraint/__init__.py
+++ dispsolver/constraint/__init__.py
@@ -3,5 +3,6 @@ from .rbe2 import RBE2HingeConstraint
 from .tie import TieConstraint
 from .penalty_hinge import PenaltyHingeConstraint
 from .contact_jax import PenaltyContactConstraint
+from .spring_hinge import SpringHingeConstraint
 
-__all__ = ["BaseConstraint", "RBE2HingeConstraint", "TieConstraint", "PenaltyHingeConstraint", "PenaltyContactConstraint"]
+__all__ = ["BaseConstraint", "RBE2HingeConstraint", "TieConstraint", "PenaltyHingeConstraint", "PenaltyContactConstraint", "SpringHingeConstraint"]
